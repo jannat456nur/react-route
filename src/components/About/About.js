08231de64b29832285ba059e4a42a9d2b3d21262
jannat456nur <jannatjenny22@gmail.com>
2021-09-28 18:04:14 -0700

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>All about</h3>
+            <p>Chololate dile koimu</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Page not found</h2>
+            <h4>Don't ask me</h4>
+        </div>
+    );
+};
+
+export default NotFound;
